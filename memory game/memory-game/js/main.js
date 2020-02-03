0671f8434167cf1a17591b712a9dae4228058f08
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-02-03 15:05:29 -0600

    created game logic

---
+++ memory game/memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!")
+let cardOne = "queen" + "of" + "diamonds";
+let cardTwo = "queen" + "of" + "hearts";
+let cardThree = "king" + "of" + "diamonds";
+let cardFour = "king" + "of" + "hearts";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
