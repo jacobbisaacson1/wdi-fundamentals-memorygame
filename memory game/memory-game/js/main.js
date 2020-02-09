12415efb8d03b68f8c68d023ac992be6e740f820
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-02-09 12:01:34 -0600

    created game logic

--- memory game/memory-game/js/main.js
+++ memory game/memory-game/js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!")
-let cardOne = "queen" + "of" + "diamonds";
-let cardTwo = "queen" + "of" + "hearts";
-let cardThree = "king" + "of" + "diamonds";
-let cardFour = "king" + "of" + "hearts";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+function checkForMatch (){
+	if (cardsInPlay[0] === cardsInPlay[1])
+{
+console.log("you found a match!");
+} else {
+	console.log("sorry, try again");
+}
+}
+function flipCard(cardId){
+	console.log("user flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+if (cardsInPlay.length === 2){
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+checkForMatch ();
